03217d90c661362d93e2e7217e327479f01fdc38
Kyle Peyton <kpeyton@nvidia.com>
2011-07-12 12:52:25 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MadeYourSiteBetterDawg::Application.config.secret_token = '994e05af2d0348c4b3e0fb3297f4f41d323dabec0ada938a59ad310e0aacc8850425bcea6165ccf52816caf45ca34c05ace37a1f3fbfea0e1c9319c64584cd2d'
